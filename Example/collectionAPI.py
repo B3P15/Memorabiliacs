7f4574ec6b481e542ac64298f4c42962f95bbbcc
ktg010 <kgilpin2022@gmail.com>
2026-01-12 15:25:53 -0600

    Made progress on logic for the live rendering of page content based on a SQL query. Began constructing general methods.

--- Example/collectionAPI.py
+++ Example/collectionAPI.py
@@ -1,12 +1,18 @@
 import streamlit as st
-
-
-#basic method that grabs
-def gatherCollectionData(collection:str, user:int):
+#basic method that grabs from mysql database to query a collection to determine what the user has
+def gatherCollectionData(user:str):
     conn = st.connection('mysql', type='sql')
-    collectionDF = conn.query(f'select * from {collection} as C where C.id = {user};') # This query is just an example to show how we can dynamically call information
-    for _, row in collectionDF.itertuples():
-        st.write(row)
+    #use st.connection.execute for inserting values into tables and whatnot
+    collectionDF = conn.query('select A.* from aruppdb.pokemon as A; ') # This query is just an example to show how we can dynamically call information
+    return collectionDF
 
+# def addToCollection()
 
+def removeCollection(tableName:str):
+    #This command is dangerous. Ensure that proper On Delete cascade is implemented
+    pass
+
+def addCollection(tableName:str):
+    conn = st.connection('mysql', type='sql')
+    conn.session.execute(f'CREATE TABLE {tableName} column1 datatype constraints, column2 datatype constraints, ...);')
 #Once the database is structured how we intend to use it, more methods cna be written to gather exactly the information that we want and format it.
--- Example/main_page.py
+++ Example/main_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from collectionAPI import gatherCollectionData
-
+from apitcg import get_cards
 st.title("Hello Streamlit-er 👋")
 st.markdown(
     """ 
@@ -19,5 +19,25 @@ st.markdown(
 if st.button("Send balloons!"):
     st.balloons()
 
+id_list = []
+
+for i in range(4):
+    id_list.append(gatherCollectionData("2").loc[i, 'CardID'])
+# set_list = ["base1-", "base3-", "bw"]
+print(id_list)
+# cardnum_list = ["3", "15", "8"]
+
+card_list = []
 #Ideally, using a correct table name and associated userid, this would generate the entire pokemon collection (including the unwanted info)
-gatherCollectionData("Pokemon", 223)
+#gatherCollectionData("Pokemon", 223)
+image_list = []
+for i in range(3):
+    card_dict = get_cards("pokemon", f"{id_list[i]}")["data"][0]
+    card_list.append("HP: "+card_dict["hp"]+"  \n"+"Type: "+card_dict["types"][0])
+    image_list.append(card_dict["images"]["small"])#
+for i in range(0, len(image_list), 3):
+    cols = st.columns(3)
+    j = 0
+    for col in cols:
+        col.image(image_list[j], width="content", caption=card_list[j])
+        j+=1
