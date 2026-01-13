0a41d7d15acc34fb32bdc3ff4feb4fcf74585559
cdw108 <coop11904@gmail.com>
2026-01-13 16:22:42 -0600

    Added home button for collection pages, added functionality for adding vinyls and removing vinyls.

--- pages/template.py
+++ pages/template.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import temp_backend as db
 
+if st.button("Home"):
+    st.switch_page("pages/home_page.py")
+
 st.title(db.current_coll, text_alignment="center")
 
 st.set_page_config(layout="wide")
@@ -8,6 +11,18 @@ st.set_page_config(layout="wide")
 
 # temp hard code for vinyls
 if (db.current_coll == "Vinyls"):
+    @st.dialog("Add Vinyl")
+    def add_vinyl():
+        name = st.text_input("Title of vinyl")
+        picture_link = st.text_input("URL of a picture of the album cover")
+        if st.button("Add to collection") and name != '' and picture_link != '':
+            db.vinyl_names.append(name.strip())
+            db.vinyl_pictures.append(picture_link.strip())
+            db.vinyl_list = dict(zip(db.vinyl_names, db.vinyl_pictures))
+            st.switch_page("pages/template.py")
+        else:
+            pass
+    
     with st.container(horizontal=True, horizontal_alignment="center"):
         for vinyl in db.vinyl_list:
 
@@ -20,6 +35,16 @@ if (db.current_coll == "Vinyls"):
                 st.image(db.vinyl_list[vinyl], width=200)
                 if st.button(f"{vinyl}", width=200):
                     vinyl_display()
+                if st.button("Remove", key=f"remove_{vinyl}", width=200):
+                    db.vinyl_names.remove(vinyl)
+                    db.vinyl_pictures.remove(db.vinyl_list[vinyl])
+                    db.vinyl_list = dict(zip(db.vinyl_names, db.vinyl_pictures))
+                    st.switch_page("pages/template.py")
+
+    with st.container(horizontal=True, horizontal_alignment="right", vertical_alignment="bottom"):
+        if st.button("Add Vinyl"):
+            add_vinyl()
+            pass
 
 
 # above but mugs
