822e06b2a2b38c8f9cce51efe11434a3cdfc5c10
ktg010 <kgilpin2022@gmail.com>
2026-01-13 13:28:24 -0600

    Began integrating code to dynamically push new user information to the database.

--- pages/login.py
+++ pages/login.py
@@ -6,12 +6,38 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+from sqlalchemy import text, Column, Integer, String, VARCHAR
+from sqlalchemy.orm import declarative_base
+from streamlit_authenticator.utilities import (CredentialsError,
+                                               ForgotError,
+                                               Hasher,
+                                               LoginError,
+                                               RegisterError,
+                                               ResetError,
+                                               UpdateError)
+
+conn = st.connection('mysql', type='sql')
+conn.session.execute(text("use user_database;"))
+conn.session.commit()
+test = conn.query('select U.UserID from Users as U;')
+st.write(test)
 
 with open('.streamlit/config.yaml') as file:
      config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
 # stauth.Hasher.hash_passwords(config['credentials'])
+
+
+Base = declarative_base()
+
+class User(Base):
+     __tablename__ = "users"
+
+     id = Column(Integer, primary_key=True)
+     UserID= Column(VARCHAR(100))
+     Username= Column(VARCHAR(100))
+     PersonalDB= Column(VARCHAR(100))
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -37,6 +63,33 @@ elif st.session_state["authentication_status"] is False:
      st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
      st.warning('Please enter your username and password')
+
+
+#widget that registers a new user
+try:
+     (email,
+     username,
+     name) = authenticator.register_user(roles=["viewer"], clear_on_submit=True, merge_username_email=True, password_hint=False)
+     if email:
+          #upon proper user registration, store user information in sql database for use as foreign key
+          #include placeholder variables to prevent against SQL injection
+          # conn.session.execute(text('use user_database; insert into users(UserID, Username, PersonalDB) values(:UserID, :Username, :PersonalDB);'), 
+          # params={"UserID":email, "Username":username, "PersonalDB":f"{username}DB"})
+          # conn.session.commit()
+
+          user = User(UserID=email, Username=username, PersonalDB=f"{name}DB")
+
+          conn.session.add(user)
+          conn.session.commit
+
+          st.success('User registered successfully')
+          print("Completed")
+except RegisterError as e:
+     st.error(e)
+
+#update the config file!
+with open('.streamlit/config.yaml', 'w') as file:
+     yaml.dump(config, file, default_flow_style=False, allow_unicode=True)
 
 #callback=st.switch_page("pages/home_page.py")
 # with st.container(horizontal=True, horizontal_alignment="center"):
